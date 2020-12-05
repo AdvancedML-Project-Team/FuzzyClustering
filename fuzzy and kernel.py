83b74544ac84bf64c514f456a57b789a9e59f4ef
KYEONGCHANLEE <cardchan@naver.com>
2020-12-06 03:30:24 +0900

    코드 정리 / 99%완료

--- fuzzy and kernel.py
+++ fuzzy and kernel.py
@@ -5,71 +5,60 @@ import matplotlib.pyplot as plt
 import os
 import time
 print(os.getcwd())
-# %% 데이터 불러오기 택1 ###########################################################
+# %% 데이터 불러오기 택1 ########################################################
 # %% 연습데이터 클러스터 2개 활모양
 df = pd.read_csv('data/2d_dataset_2.csv')
 df = df.iloc[:, 1:]
 df = np.asarray(df)
-# %% 사이킷런 링 데이터
-df, y = datasets.make_circles(n_samples=400, factor=.1, noise=.1)
+plt.scatter(df[:, 0], df[:, 1])
+# %% 사이킷런 링 데이터 (과녁형)
+df, y = datasets.make_circles(n_samples=400, factor=.1, noise=.1, random_state=42)
+plt.scatter(df[:, 0], df[:, 1])
+# %% 사이킷런 링 데이터 (고리 2개)
+df, y = datasets.make_circles(n_samples=600, factor=.5, noise=.05, random_state=42)
+plt.scatter(df[:, 0], df[:, 1])
+# %% 사이킷런 교차 데이터
+df, y = datasets.make_classification(n_samples=300, n_features=2, n_redundant=0, n_informative=2,
+                             n_clusters_per_class=1, random_state=(1))
+plt.scatter(df[:, 0], df[:, 1])
 # %% 사이킷런 5 blobs 데이터
 centers = [[0, 0], [0.7, 0.7], [-0.7, 0.7], [0.7, -0.7], [-0.7, -0.7]]
 df, y = datasets.make_blobs(n_samples=400, n_features=2, centers=centers, cluster_std=0.15)
+plt.scatter(df[:, 0], df[:, 1])
 # %% 불균형데이터
 df = pd.read_csv('data/practice.txt', sep=' ', header=None)
 df = np.asarray(df)
+plt.scatter(df[:, 0], df[:, 1])
 # %% 여러 군집형 데이터
 df = pd.read_csv('data/Aggregation.txt', sep='\t', header=None)
 df = df.iloc[:, :-1]
 df = np.asarray(df)
+plt.scatter(df[:, 0], df[:, 1])
 # %% 연습데이터 클러스터 2개
 df = pd.read_csv('data/2d_dataset_1.csv')
 df = df.iloc[:, 1:]
 df = np.asarray(df)
+plt.scatter(df[:, 0], df[:, 1])
 # %% 연습데이터 클러스터 2개 활모양
 df = pd.read_csv('data/2d_dataset_2.csv')
 df = df.iloc[:, 1:]
 df = np.asarray(df)
+plt.scatter(df[:, 0], df[:, 1])
 # %% 연습데이터 클러스터 2개 고리모양
 df = pd.read_csv('data/2d_dataset_3.csv')
 df = df.iloc[:, 1:]
 df = np.asarray(df)
+plt.scatter(df[:, 0], df[:, 1])
 ###############################################################################
 
-# %% parameter 세팅 ############################################################
-N = df.shape[0]
-p = df.shape[1]
-plt.scatter(df[:,0], df[:,1])
-###############################################################################
-
-# %% 중심점 초기화 및 Kernel함수 정의 ############################################
-# 3) Initialize centroids
-K = 5
-m = 2
-init_ctr_idx = np.random.choice(N, K)
-ctr = df[init_ctr_idx]
+# %% 함수 정의 #################################################################
 
 
-def Kernel(X, Y, b=0.5, d=2):
+def Kernel(X, Y, b=.5, d=2):
     if X.ndim > 1 and Y.ndim > 1:
         return (np.dot(X, Y.T) + b) ** d
 
     return (np.dot(X, Y) + b) ** d
-
-# 4) Cumpute the degree of membership of all feature vectors in
-# all the clusters uik
-
-
-D = np.zeros((N, K))
-eps = 10e-5
-
-for i in range(N):
-    for k in range(K):
-        d_square = Kernel(df[i], df[i]) - 2 * Kernel(df[i], ctr[k]) +\
-            Kernel(ctr[k], ctr[k])
-        D[i, k] = (1 / (d_square + eps)) ** (1 / (m - 1))
-row_sums = D.sum(axis=1)
-prev_U = D / row_sums[:, np.newaxis]
 
 # 5-1) Compute new kernel matrix K(Xi, Vk) and K(Vj_hat, Vj_hat)
 
@@ -88,45 +77,12 @@ def Kernel_newc_newc(k, U):
 
     return denominator / numerator
 
-###############################################################################
 
-# %% 반복구간 #####################################################################
-# 5-2) Update the degree of membership uik to uik_hat according to 4)
+# silhouette 구하기 함수
+def get_silhouette(df, K, y_pred):
+    N = df.shape[0]
+    p = df.shape[1]
 
-
-change = 1
-it = 1
-while((change > 0.01) and it < 30):
-    print('#### iter : {}/{} ####'.format(it, N))
-    D = np.zeros((N, K))
-    for i in range(N):
-        if i % 100 == 0:
-            print('i:{}...'.format(i))
-        for k in range(K):
-            d_square =\
-                Kernel(df[i], df[i]) -\
-                2 * Kernel_xi_newc(df[i], k, prev_U) +\
-                Kernel_newc_newc(k, prev_U)
-
-            D[i, k] = (1 / d_square) ** (1 / (m - 1))
-    row_sums = D.sum(axis=1)
-    curr_U = D / row_sums[:, np.newaxis]
-    change = max(np.linalg.norm(prev_U - curr_U, axis=1))
-
-    print('max(|U - U\'|) : {}'.format(change))
-    prev_U = curr_U
-    it += 1
-###############################################################################
-
-# %% 2차원 데이터 scatter로 확인 #################################################
-
-y_pred = np.argmax(curr_U, axis=1)
-plt.scatter(df[:, 0], df[:, 1], c=y_pred)
-
-###############################################################################
-
-# %% k값 바꾸면서 elbow 그리기(오래걸림주의) ######################################
-def get_silhouette(df, y_pred):
     si_sum = 0
     for i in range(N):
         samples_in_cluster = df[np.argwhere(y_pred == y_pred[i]).ravel()]
@@ -137,16 +93,23 @@ def get_silhouette(df, y_pred):
             bis.append(np.mean(np.linalg.norm(df[i] - samples_in_other_cluster, axis=1)))
         bi = min(bis)
         si_sum += (bi - ai) / max(ai, bi)
-    
+
     return si_sum / N
 
-def predict(df, K, m=2, maxiter = 30, epsilon = 0.05):
+
+# 예측 함수
+def predict(df, K, m=2, maxiter=30, epsilon=0.01):
+    N = df.shape[0]
+    p = df.shape[1]
+
+    # 3) Initialize centroids
     init_ctr_idx = np.random.choice(N, K)
     ctr = df[init_ctr_idx]
+
     # 4) Cumpute the degree of membership of all feature vectors in all the clusters uik
     D = np.zeros((N, K))
     eps = 10e-5
-    
+
     for i in range(N):
         for k in range(K):
             d_square = Kernel(df[i], df[i]) - 2 * Kernel(df[i], ctr[k]) +\
@@ -154,8 +117,9 @@ def predict(df, K, m=2, maxiter = 30, epsilon = 0.05):
             D[i, k] = (1 / (d_square + eps)) ** (1 / (m - 1))
     row_sums = D.sum(axis=1)
     prev_U = D / row_sums[:, np.newaxis]
-    
+
     # 5-2) Update the degree of membership uik to uik_hat according to 4)
+    # 반복구간
     change = 1
     it = 1
     while((change > epsilon) and it < maxiter):
@@ -163,212 +127,77 @@ def predict(df, K, m=2, maxiter = 30, epsilon = 0.05):
         D = np.zeros((N, K))
         for i in range(N):
             if i % 100 == 0:
-                print('i : {:>3}/{}'.format(i,N))
+                print('i : {:>5}/{}'.format(i, N))
             for k in range(K):
                 d_square =\
                     Kernel(df[i], df[i]) -\
                     2 * Kernel_xi_newc(df[i], k, prev_U) +\
                     Kernel_newc_newc(k, prev_U)
-                    
+
                 D[i, k] = (1 / d_square) ** (1 / (m - 1))
         row_sums = D.sum(axis=1)
         curr_U = D / row_sums[:, np.newaxis]
         change = max(np.linalg.norm(prev_U - curr_U, axis=1))
-        print()
         print('change : {}'.format(change))
+        print()
         prev_U = curr_U
         it += 1
     y_pred = np.argmax(curr_U, axis=1)
-    
+
     return y_pred
 #%% k값 하나로 예측할 때 ########################################################
-K = 4
+
+K = 2
 m = 2
 y_pred = predict(df, K=K, m=m)
 plt.scatter(df[:, 0], df[:, 1], c=y_pred)
+get_silhouette(df, K, y_pred)
+###############################################################################
 
-#%% k값 여러개로 그림그릴때(기본설정 k:1~10 ######################################
+#%% k값 여러개로 그림그릴때(기본설정 k:2~10) #####################################
+
+m = 2
 ks = []
 silhouettes = []
 y_preds = []
-for k in range(1, 10+1):
+for k in range(2, 10+1):
     print('*************************************k : {}'.format(k))
     y_pred = predict(df, k)
     ks.append(k)
-    silhouettes.append(get_silhouette(df, y_pred))
+    silhouettes.append(get_silhouette(df, k, y_pred))
     y_preds.append(y_pred)
-    
-plt.scatter(ks, silhouettes)
-#%% y_preds로 그림그리기 #######################################################
-plt.scatter(df[:,0], df[:,1], c=y_preds[0])
-plt.scatter(df[:,0], df[:,1], c=y_preds[1])
-plt.scatter(df[:,0], df[:,1], c=y_preds[2])
-plt.scatter(df[:,0], df[:,1], c=y_preds[3])
-plt.scatter(df[:,0], df[:,1], c=y_preds[4])
-plt.scatter(df[:,0], df[:,1], c=y_preds[5])
-plt.scatter(df[:,0], df[:,1], c=y_preds[6])
-plt.scatter(df[:,0], df[:,1], c=y_preds[7])
-plt.scatter(df[:,0], df[:,1], c=y_preds[8])
-plt.scatter(df[:,0], df[:,1], c=y_preds[9])
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+plt.scatter(ks, silhouettes) #  Elbow 그래프
+plt.xlabel('군집 수 K')
+plt.ylabel('Silhouette')
+###############################################################################
+
+#%% (여러개그리기 연속)y_preds로 그림그리기 ######################################
+#%%
+plt.scatter(df[:, 0], df[:, 1], c=y_preds[0])  # k=2
+plt.title('K = 2')
+#%%
+plt.scatter(df[:, 0], df[:, 1], c=y_preds[1])  # k=3
+plt.title('K = 3')
+#%%
+plt.scatter(df[:, 0], df[:, 1], c=y_preds[2])  # k=4
+plt.title('K = 4')
+#%%
+plt.scatter(df[:, 0], df[:, 1], c=y_preds[3])  # k=5
+plt.title('K = 5')
+#%%
+plt.scatter(df[:, 0], df[:, 1], c=y_preds[4])  # k=6
+plt.title('K = 6')
+#%%
+plt.scatter(df[:, 0], df[:, 1], c=y_preds[5])  # k=7
+plt.title('K = 7')
+#%%
+plt.scatter(df[:, 0], df[:, 1], c=y_preds[6])  # k=8
+plt.title('K = 8')
+#%%
+plt.scatter(df[:, 0], df[:, 1], c=y_preds[7])  # k=9
+plt.title('K = 9')
+#%%
+plt.scatter(df[:, 0], df[:, 1], c=y_preds[8])  # k=10
+plt.title('K = 10')
+###############################################################################
--- fuzzy.py
+++ fuzzy.py
@@ -1,31 +1,72 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Nov 13 02:44:40 2020
-
-@author: Admin
-"""
-
 import pandas as pd
 import numpy as np
 from sklearn import datasets
 import matplotlib.pyplot as plt
 import seaborn as sns
-#%% 사이킷런 링 데이터
-df, y = datasets.make_circles(n_samples=400, factor=.1, noise=.1)
-N = df.shape[0]
-p = df.shape[1]
-plt.scatter(df[:,0], df[:,1])
-#%% get_centroid(weight)
+import os
+import time
+print(os.getcwd())
+# %% 데이터 불러오기 택1 ########################################################
+# %% 연습데이터 클러스터 2개 활모양
+df = pd.read_csv('data/2d_dataset_2.csv')
+df = df.iloc[:, 1:]
+df = np.asarray(df)
+plt.scatter(df[:, 0], df[:, 1])
+# %% 사이킷런 링 데이터 (과녁형)
+df, y = datasets.make_circles(n_samples=400, factor=.1, noise=.1, random_state=42)
+plt.scatter(df[:, 0], df[:, 1])
+# %% 사이킷런 링 데이터 (고리 2개)
+df, y = datasets.make_circles(n_samples=600, factor=.5, noise=.05, random_state=42)
+plt.scatter(df[:, 0], df[:, 1])
+# %% 사이킷런 교차 데이터
+df, y = datasets.make_classification(n_samples=300, n_features=2, n_redundant=0, n_informative=2,
+                             n_clusters_per_class=1, random_state=(1))
+plt.scatter(df[:, 0], df[:, 1])
+# %% 사이킷런 5 blobs 데이터
+centers = [[0, 0], [0.7, 0.7], [-0.7, 0.7], [0.7, -0.7], [-0.7, -0.7]]
+df, y = datasets.make_blobs(n_samples=400, n_features=2, centers=centers, cluster_std=0.15)
+plt.scatter(df[:, 0], df[:, 1])
+# %% 불균형데이터
+df = pd.read_csv('data/practice.txt', sep=' ', header=None)
+df = np.asarray(df)
+plt.scatter(df[:, 0], df[:, 1])
+# %% 여러 군집형 데이터
+df = pd.read_csv('data/Aggregation.txt', sep='\t', header=None)
+df = df.iloc[:, :-1]
+df = np.asarray(df)
+plt.scatter(df[:, 0], df[:, 1])
+# %% 연습데이터 클러스터 2개
+df = pd.read_csv('data/2d_dataset_1.csv')
+df = df.iloc[:, 1:]
+df = np.asarray(df)
+plt.scatter(df[:, 0], df[:, 1])
+# %% 연습데이터 클러스터 2개 활모양
+df = pd.read_csv('data/2d_dataset_2.csv')
+df = df.iloc[:, 1:]
+df = np.asarray(df)
+plt.scatter(df[:, 0], df[:, 1])
+# %% 연습데이터 클러스터 2개 고리모양
+df = pd.read_csv('data/2d_dataset_3.csv')
+df = df.iloc[:, 1:]
+df = np.asarray(df)
+plt.scatter(df[:, 0], df[:, 1])
+###############################################################################
+
+#%% 함수정의 ###################################################################
+
+# get_centroid(weight)
 def get_centroid(weight, K):
     centroid = []
     for k in range(K):
-        wk = weight[:,k] ** m
+        wk = weight[:, k] ** m
         denominator = df * wk[:, None]
         centroid.append(np.sum(denominator, axis=0) / np.sum(wk))
     return centroid
-#%% def update_weight(centroid)
+
+
+# def update_weight(centroid)
 def update_weight(centroid, N, K):
-    weight = np.zeros((N,K))
+    weight = np.zeros((N, K))
     for i in range(N):
         for j in range(K):
             xi = df[i]
@@ -33,40 +74,37 @@ def update_weight(centroid, N, K):
             for k in range(K):
                 summation += (np.linalg.norm(xi-centroid[j]) / np.linalg.norm(xi-centroid[k])) ** (2/(m-1))
             weight[i, j] = 1 / summation
-    
+
     return weight
 
-#%% 반복구간
 
-
-def predict(df, K=3, m=2, epsilon=0.05, max_iter=50):
-    print(K)
+# 예측함수
+def predict(df, K=3, m=2, epsilon=0.01, max_iter=50):
     N = df.shape[0]
     weight = np.random.rand(N, K)
     weight /= weight.sum(axis=1, keepdims=1)
     prev_centroid = []
     for k in range(K):
         prev_centroid.append(df[k])
-    cnt = 1
-    change = [100] * K
 
-    while (sum(np.asarray(change) > epsilon) > 0) or (max_iter >= cnt):
-        # print('iter:{}'.format(cnt))
+    # 반복구간
+    cnt = 1
+    change_max = 100
+    while (change_max > epsilon) and (max_iter > cnt):
         curr_centroid = get_centroid(weight, K)
         change = []
         for k in range(K):
             change.append(np.linalg.norm(curr_centroid[k] - prev_centroid[k]))
-        # print('center change:{}'.format(change))
+        change_max = max(change)
         weight = update_weight(curr_centroid, N, K)
         prev_centroid = curr_centroid
         cnt += 1
     y_pred = np.argmax(weight, axis=1)
-    
+
     return y_pred, weight, curr_centroid
 
-#%% cost 구하기
 
-
+# cost(SSE) 구하기
 def get_cost(df, y_pred, curr_centroid):
     N = df.shape[0]
     p = df.shape[1]
@@ -74,90 +112,122 @@ def get_cost(df, y_pred, curr_centroid):
     for i in range(N):
         cluster = y_pred[i]
         clusters[i] = curr_centroid[cluster]
-    cost = np.sum(np.linalg.norm(df - clusters, axis = 1) ** 2)
-    
+    cost = np.sum(np.linalg.norm(df - clusters, axis=1) ** 2)
+
     return cost
-#%% 5000 by 2 연습데이터 클러스터 15개
-df = pd.read_csv('data/S-sets.csv', header=None)
-df = np.asarray(df)
-N = df.shape[0]
-p = df.shape[1]
-plt.scatter(df[:,0], df[:,1])
 
-#%% wine 데이터 
-df = pd.read_csv('data/wine-clustering.csv')
-df = np.asarray(df)
-N = df.shape[0]
-p = df.shape[1]
-#%% iris 데이터
-iris = datasets.load_iris()
-df = pd.DataFrame(iris['data'])
-df = np.asarray(df)
-N = df.shape[0]
-p = df.shape[1]
-#%% 연습데이터 클러스터 2개
-df = pd.read_csv('data/2d_dataset_1.csv')
-df = df.iloc[:, 1:]
-df = np.asarray(df)
-N = df.shape[0]
-p = df.shape[1]
-plt.scatter(df[:,0], df[:,1])
-#%% 연습데이터 클러스터 2개 활모양
-df = pd.read_csv('data/2d_dataset_2.csv')
-df = df.iloc[:, 1:]
-df = np.asarray(df)
-N = df.shape[0]
-p = df.shape[1]
-plt.scatter(df[:,0], df[:,1])
-#%% 여러 군집형 데이터
 
-df = pd.read_csv('data/Aggregation.txt', sep='\t', header=None)
-df = df.iloc[:, :-1]
-df = np.asarray(df)
-N = df.shape[0]
-p = df.shape[1]
-plt.scatter(df[:,0], df[:,1])
+# silhouette 구하기 함수
+def get_silhouette(df, K, y_pred):
+    N = df.shape[0]
 
-#%% 3차원 이상 pairplot으로 확인
-plot_df = pd.DataFrame(df)
-plot_df['y'] = y_pred
-sns.pairplot(plot_df, hue='y')
-#%% 2차원 scatter로 확인
-plt.scatter(df[:, 0], df[:, 1], c=y_pred)
-for k in range(K):
-    plt.scatter(curr_centroid[k][0], curr_centroid[k][1], s=100)
-    
-    
-#%% k한개돌릴때
+    si_sum = 0
+    for i in range(N):
+        samples_in_cluster = df[np.argwhere(y_pred == y_pred[i]).ravel()]
+        ai = np.mean(np.linalg.norm(df[i] - samples_in_cluster, axis=1))
+        bis = []
+        for k in [_ for _ in range(K) if _ != y_pred[i]]:
+            samples_in_other_cluster = df[np.argwhere(y_pred == k).ravel()]
+            bis.append(np.mean(np.linalg.norm(df[i] - samples_in_other_cluster, axis=1)))
+        bi = min(bis)
+        si_sum += (bi - ai) / max(ai, bi)
 
-plt.scatter(df[:,0], df[:,1])
+    return si_sum / N
+###############################################################################
+
+#%% k한개돌릴때 ################################################################
+
 K = 2
 m = 2
-epsilon = 0.05
+epsilon = 0.01
 max_iter = 50
-y_pred, weight, curr_centroid = predict(df, K)
+y_pred, weight, curr_centroid = predict(df, K, epsilon=epsilon, max_iter=max_iter)
 
+plt.scatter(df[:, 0], df[:, 1], c=y_pred)  # 그림확인
+for k in range(K):  # centroid 그리기
+    plt.scatter(curr_centroid[k][0], curr_centroid[k][1], s=100)
+###############################################################################
 
+#%% k 여러개 돌릴때(기본 2~20) ##################################################
 
-
-
-
-#%% k 여러개 돌릴때
-
-plt.scatter(df[:,0], df[:,1])
-K = 2
 m = 2
-epsilon = 0.05
+epsilon = 0.01
 max_iter = 50
 
 ks = []
 costs = []
-for k in range(1, 10+1):
-    y_pred, weight, curr_centroid = predict(df, K=k)
+y_preds = []
+
+max_K = 20
+for k in range(2, max_K+1):
+    print('K : {:>2}/{}'.format(k, max_K))
+    y_pred, weight, curr_centroid = predict(df, k, epsilon=epsilon, max_iter=max_iter)
     ks.append(k)
-    costs.append(get_cost(df, y_pred, curr_centroid))
+    # costs.append(get_cost(df, y_pred, curr_centroid))  # cost 둘중 아무거나 써도됨
+    costs.append(get_silhouette(df, k, y_pred))
+    y_preds.append(y_pred)
 
+plt.scatter(ks, costs)  # Elbow 그래프
+plt.xlabel('군집 수 K')
+plt.ylabel('Silhouette')
+###############################################################################
+
+#%% (여러개그리기 연속)y_preds로 그림그리기 ######################################
 #%%
-plt.scatter(ks, costs)
-
-
+plt.scatter(df[:, 0], df[:, 1], c=y_preds[0])  # k=2
+plt.title('K = 2')
+#%%
+plt.scatter(df[:, 0], df[:, 1], c=y_preds[1])  # k=3
+plt.title('K = 3')
+#%%
+plt.scatter(df[:, 0], df[:, 1], c=y_preds[2])  # k=4
+plt.title('K = 4')
+#%%
+plt.scatter(df[:, 0], df[:, 1], c=y_preds[3])  # k=5
+plt.title('K = 5')
+#%%
+plt.scatter(df[:, 0], df[:, 1], c=y_preds[4])  # k=6
+plt.title('K = 6')
+#%%
+plt.scatter(df[:, 0], df[:, 1], c=y_preds[5])  # k=7
+plt.title('K = 7')
+#%%
+plt.scatter(df[:, 0], df[:, 1], c=y_preds[6])  # k=8
+plt.title('K = 8')
+#%%
+plt.scatter(df[:, 0], df[:, 1], c=y_preds[7])  # k=9
+plt.title('K = 9')
+#%%
+plt.scatter(df[:, 0], df[:, 1], c=y_preds[8])  # k=10
+plt.title('K = 10')
+#%%
+plt.scatter(df[:, 0], df[:, 1], c=y_preds[9])  # k=11
+plt.title('K = 11')
+#%%
+plt.scatter(df[:, 0], df[:, 1], c=y_preds[10])  # k=12
+plt.title('K = 12')
+#%%
+plt.scatter(df[:, 0], df[:, 1], c=y_preds[11])  # k=13
+plt.title('K = 13')
+#%%
+plt.scatter(df[:, 0], df[:, 1], c=y_preds[12])  # k=14
+plt.title('K = 14')
+#%%
+plt.scatter(df[:, 0], df[:, 1], c=y_preds[13])  # k=15
+plt.title('K = 15')
+#%%
+plt.scatter(df[:, 0], df[:, 1], c=y_preds[14])  # k=16
+plt.title('K = 16')
+#%%
+plt.scatter(df[:, 0], df[:, 1], c=y_preds[15])  # k=17
+plt.title('K = 17')
+#%%
+plt.scatter(df[:, 0], df[:, 1], c=y_preds[16])  # k=18
+plt.title('K = 18')
+#%%
+plt.scatter(df[:, 0], df[:, 1], c=y_preds[17])  # k=19
+plt.title('K = 19')
+#%%
+plt.scatter(df[:, 0], df[:, 1], c=y_preds[18])  # k=20
+plt.title('K = 20')
+###############################################################################
